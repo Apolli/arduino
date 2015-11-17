6304474a07b20e94ea29ce38279d0bd34f005452
ahembach <ahembach@silversurfer7.de>
2015-11-17 15:26:09 +0100

    cleanup

--- display_nrf24_recive/display_nrf24_recive.ino
+++ display_nrf24_recive/display_nrf24_recive.ino
@@ -5,20 +5,24 @@
 #include <SPI.h>
 #include <stdlib.h>
 
+// define ip
 #define SERVER_ADDRESS 50
-// Singleton instance of the radio driver
-RH_NRF24 driver;
-// RH_NRF24 driver(8, 7);   // For RFM73 on Anarduino Mini
-// Class to manage message delivery and receipt, using the driver declared above
-RHReliableDatagram manager(driver, SERVER_ADDRESS);
 
+// init radioHead
+RH_NRF24 driver;
+RHReliableDatagram manager(driver, SERVER_ADDRESS);
+uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+
+// init display
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
+// init tank variables
 float tankHeight = 1.50;
 float tankWidth = 1.25;
 float tankLength = 1.84;
 float tankOffset = 0.40;
 
+// init battery icon
 byte bat[8] = {
   0b00100,
   0b11011,
@@ -30,149 +34,138 @@ byte bat[8] = {
   0b11111
 };
 
+// init timer for display updates
+unsigned long timer;
+
+// init message queue for json strings
+char* messageQueue[200] = {};
+int currentContent;
+
 void setup() 
 {
   Serial.begin(9600);
-  if (!manager.init())
+  if (!manager.init()) {
     Serial.println("init failed");
+  }
 
+  // init display
   lcd.begin(20, 4);
-
   lcd.setCursor(0, 0);
   lcd.print("Ready");
 }
 
-// Dont put this on the stack:
-uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
-
-unsigned long timer;
-char* messageQueue[200] = {};
-
-
 void loop()
 {
-
-  
+  // check for new messages
   if (manager.available())
   {
-    // Wait for a message addressed to us from the client
     uint8_t len = sizeof(buf);
     uint8_t from;
     if (manager.recvfromAck(buf, &len, &from))
     {
-//      Serial.println((char*)buf);
-
       messageQueue[(int)from] = (char*)buf;
 
       Serial.println("");      
       Serial.println("");      
       Serial.println("update queue data:");
       for (int i = 0; i < 200; i++){
-          if(messageQueue[i]) {
-              Serial.println(messageQueue[i]);
-          }
+        if(messageQueue[i]) {
+          Serial.print("IP:"+String(i)+", Json: ");
+          Serial.println(messageQueue[i]);
+        }
       }   
-      Serial.println("");      
-        
+      Serial.println("");
     }
   }
-  
-  
-  
+
+
+  // update display content and rotate messages
   checkContent();
-  
 
-
-/*
-  if(((millis()-timer) / 1000) > 240) {
-    String message;  
-    message = String((millis()-timer) / 1000 )+" sek";
-    message += " / Data error ";
-    
-    lcd.setCursor(0, 3);
-    lcd.print(message);
-  }*/
+  // update timer
+  timer = millis();
 }
 
-int currentContent;
+
 
 int getNextAcive()
 {
-    // get next active
-    if(currentContent) {
-      for (int i = 0; i < 200; i++) {
-          if(i > currentContent && messageQueue[i]) {
-              return i;
-          }
+  // get next active
+  if(currentContent) {
+    for (int i = 0; i < 200; i++) {
+      if(i > currentContent && messageQueue[i]) {
+        return i;
       }
     }
-    
-    // no active found... get first entry
-    for (int i = 0; i < 200; i++) {
-        if(messageQueue[i]) {
-            return i;
-        }
+  }
+  
+  // no active found... get first entry
+  for (int i = 0; i < 200; i++) {
+    if(messageQueue[i]) {
+      return i;
     }
-    
+  }
 }
 
 
 void checkContent()
 {
-    StaticJsonBuffer<256> jsonBuffer;
-    int lvl;
-    String sensor;
-    float realLvl;
-    String message;
-    int battery;
-    char temp[10];
-    char tmp[10];
-    char hum[10];
-    int soil;
-    
-    if(((millis()-timer) / 1000) > 5) {
-        int from = getNextAcive();
-        if(!messageQueue[from]) {
-          return;
-        }
-        
-//        Serial.println("next active:");
-//        Serial.println(from);
+  // declare some variables
+  StaticJsonBuffer<256> jsonBuffer;
 
-        JsonArray& array = jsonBuffer.parseArray(messageQueue[(int)from]);
-        currentContent = from;
+  int lvl;
+  int battery;
+  int soil;
 
+  float realLvl;
 
-        
-        timer = millis();
+  String sensor;
+  String message;
   
-        lcd.clear();
-        
-        switch(from) {
+  char temp[10];
+  char tmp[10];
+  char hum[10];
+
+
+  // rotate every 5 seconds
+  if(((millis()-timer) / 1000) > 5) {
+    int from = getNextAcive();
+    if(!messageQueue[from]) {
+      return;
+    }
+
+    // get new message
+    JsonArray& array = jsonBuffer.parseArray(messageQueue[(int)from]);
+    currentContent = from;
+
+    // remove old message
+    lcd.clear();
+
+    switch(from) {
           case 3: // water 
             lvl = array[0];
             realLvl = getLvl(lvl);
-            
+
             Serial.println(realLvl);
-            
+
             message = "Prozent: "+getPercent(realLvl)+"%";
             lcd.setCursor(0, 0);
             lcd.print(message);
-    
+
             message = "Liter: "+getLiter(realLvl);
             lcd.setCursor(0, 1);
             lcd.print(message);
-    
+
             message = "Wasserstand: "+getHeight(realLvl)+"cm";
             lcd.setCursor(0, 2);
             lcd.print(message);
             break;
-            
+          
           case 1: // room
-            battery = array[2];
             dtostrf(array[0], 1, 1, tmp);
             dtostrf(array[1], 1, 1, hum);
-            
+            battery = array[2];
+
             lcd.createChar(0, bat);
             message = "Temp. innen      "+String(battery)+"%";
             lcd.setCursor(0, 0);
@@ -187,11 +180,11 @@ void checkContent()
             lcd.setCursor(0, 1);
             lcd.print(message);
             break;
+
           case 6: // outdoor
-            
-            battery = array[2];
             dtostrf(array[0], 1, 1, temp);
             dtostrf(array[1], 1, 1, hum);
+            battery = array[2];
             soil = array[3];
             
             lcd.createChar(0, bat);
@@ -213,12 +206,11 @@ void checkContent()
             lcd.print(message);
             break;
         }
+      }
     }
-}
 
 
 // helper methods
-
 float getLvl(float lvl) {
   float waterLevel;  
   lvl = lvl/100;
@@ -243,7 +235,6 @@ String getLiter(float realLvl) {
   Serial.println(realLvl);  
   return String(l);
 }
-
 
 String getPercent(float realLvl) {
   int p = 100 / (tankHeight - tankOffset) * realLvl;
